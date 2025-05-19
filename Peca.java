c6b5100bfff33f1d964781fb013f544b56f397e2
otrebormor <otrebormor@gmail.com>
2025-05-19 15:18:54 -0300

    Add files via upload

---
+++ Peca.java
@@ -0,0 +1,36 @@
+package classes;
+
+public abstract class Peca {
+	private String descricao;
+	private int quantidade;
+	private int estoqueMinimo;
+	private int estoqueMaximo;
+
+	public Peca(String descricao, int quantidade, int estoqueMinimo, int estoqueMaximo) {
+		this.descricao = descricao;
+		this.quantidade = quantidade;
+		this.estoqueMinimo = estoqueMinimo;
+		this.estoqueMaximo = estoqueMaximo;
+	}
+
+	public abstract void Venda(int numeroItens);
+
+	public void reposicaoEstoque() {
+		if (this.quantidade < this.estoqueMinimo) {
+			this.quantidade = this.estoqueMaximo;
+		}
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade -= quantidade;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
