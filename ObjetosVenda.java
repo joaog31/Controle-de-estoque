e22320ef58e37c6328266813b51d7efaeb9394ca
otrebormor <otrebormor@gmail.com>
2025-05-25 14:26:23 -0300

    Add files via upload
    Classe interface

---
+++ ObjetosVenda.java
@@ -0,0 +1,16 @@
+package classes;
+
+import java.util.Scanner;
+
+public interface ObjetosVenda {
+	Scanner ler = new Scanner(System.in);
+
+	void venda();
+
+	void reposicaoEstoque();
+
+	void informacoesEstoque();
+
+	boolean testeVenda(int itensVenda);
+
+}
