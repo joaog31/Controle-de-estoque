ed6e580d99d104fb9d21af31f849e9b3e7a8df1f
otrebormor <otrebormor@gmail.com>
2025-05-28 14:35:29 -0300

    Update Peca.java

--- Peca.java
+++ Peca.java
@@ -1,6 +1,6 @@
 package classes;
 
-public abstract class Peca implements Item {// A implementação de ObjetosVenda normaliza a classe com base na interface.
+public abstract class Peca implements Item {// A implementação de Item que normaliza a classe com base na interface.
 	private String descricao;
 	private int quantidadeAtual;
 	private int estoqueMinimo;
@@ -22,6 +22,7 @@ public abstract class Peca implements Item {// A implementação de ObjetosVenda
 	public abstract void venda();
 
 	public void setQuantidadeAtual(int itensVendidos) {// Atualiza a quantidade de itens em estoque atual.
+
 		this.quantidadeAtual -= itensVendidos;
 	}
 
---
+++ RoupaPMG.java
@@ -0,0 +1,62 @@
+package classes;
+
+import java.util.Scanner;
+
+public class RoupaPMG implements Item{
+  private String descricao;
+  private int quantidadeP;
+  private int quantidadeM;
+  private int quantidadeG;
+  private int estoqueMinimo;
+  private int estoqueMaximo;
+
+  public RoupaPMG(String descricao ,int quantidadeP, int quantidadeG, int quantidadeM, int estoqueMinimo, int estoqueMaximo) {
+      this.descricao= descricao;
+      this.quantidadeG = quantidadeG;
+      this.quantidadeM = quantidadeM;
+      this.quantidadeP = quantidadeP;
+      this.estoqueMinimo = estoqueMinimo;
+      this.estoqueMaximo = estoqueMaximo;
+
+  }
+
+  public String getDescricao() {
+      return descricao;
+  }
+
+    @Override
+    public void venda() {
+      Scanner ler = new Scanner(System.in);
+      String tamanho;
+      System.out.println("Escolha qual tamanho de roupa você quer comprar,digite P, M ou  G: ");
+      tamanho = ler.nextLine();
+
+      if (tamanho.equals("P") || tamanho.equals("p")) {
+          this.quantidadeP -=;
+
+      } else if (tamanho.equals("M") || tamanho.equals("m")) {
+          this.quantidadeM -=;
+
+      } else if (tamanho.equals("G") || tamanho.equals("g")) {
+          this.quantidadeG -=;
+      }
+
+
+
+    }
+
+    @Override
+    public void reposicaoEstoque() {
+
+    }
+
+    @Override
+    public void informacoesEstoque() {
+
+    }
+
+    @Override
+    public boolean testeVenda(int itensVenda) {
+        return false;
+    }
+}
