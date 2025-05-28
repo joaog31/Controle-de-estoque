c4af90e5eda29bed964c3f42b1e04a78ab42b377
otrebormor <otrebormor@gmail.com>
2025-05-28 14:17:34 -0300

    Update ObjetosVenda.java
    Atualização com comentários

--- ObjetosVenda.java
+++ ObjetosVenda.java
@@ -2,17 +2,16 @@ package classes;
 
 import java.util.Scanner;
 
-public interface ObjetosVenda {
+public interface ObjetosVenda {// Essa é a classe base para o projeto. a partir dela é possível implementar um vetor e fazer polimosfismo 
 	Scanner ler = new Scanner(System.in);
 
-	void venda();
+	void venda(); // função venda que é implementada em todas as subclasses, dessa forma, o comando venda chamará a venda do objeto instanciado.
 
-	void reposicaoEstoque();
+	void reposicaoEstoque(); //função reposicaoEstoque também terá mesma simplicidades da venda.
 
-	void informacoesEstoque();
+	void informacoesEstoque(); //função informacoesEstoque também terá mesma simplicidades da venda.
 
-	boolean testeVenda(int itensVenda);
-
-	String getDescricao();
-
+	boolean testeVenda(int itensVenda); //função testeVenda trabalha junto com venda, e faz o teste para determinar se a venda é possível diante do estoque.
+	
+	String getDescricao(); //Função é usada para criar o "menu de vendas", por isso deve ser implementado em todas as classes.
 }
