1c619896d1ccb6382735146ca2b09f7ee3284900
Ycla <ycla.paiva@ucsal.edu.br>
2025-06-04 11:23:01 -0300

    Add files via upload

---
+++ Controle-de-estoque-main/Item.java
@@ -0,0 +1,17 @@
+package classes;
+
+import java.util.Scanner;
+
+public interface Item {// Essa é a classe base para o projeto. a partir dela é possível implementar um vetor e fazer polimosfismo 
+	Scanner ler = new Scanner(System.in);
+
+	void venda(); // função venda que é implementada em todas as subclasses, dessa forma, o comando venda chamará a venda do objeto instanciado.
+
+	void reposicaoEstoque(); //função reposicaoEstoque também terá mesma simplicidades da venda.
+
+	void informacoesEstoque(); //função informacoesEstoque também terá mesma simplicidades da venda.
+
+	boolean testeVenda(int itensVenda); //função testeVenda trabalha junto com venda, e faz o teste para determinar se a venda é possível diante do estoque.
+	
+	String getDescricao(); //Função é usada para criar o "menu de vendas", por isso deve ser implementado em todas as classes.
+}
---
+++ Controle-de-estoque-main/Peca.java
@@ -0,0 +1,45 @@
+package classes;
+
+public abstract class Peca implements Item {// A implementação de Item que normaliza a classe com base na interface.
+	private String descricao;
+	private int quantidadeAtual;
+	private int estoqueMinimo;
+	private int estoqueMaximo;
+
+	public Peca(String descricao, int quantidadeAtual, int estoqueMinimo, int estoqueMaximo) {
+		this.descricao = descricao;
+		this.quantidadeAtual = quantidadeAtual;
+		this.estoqueMinimo = estoqueMinimo;
+		this.estoqueMaximo = estoqueMaximo;
+	}
+
+	public void reposicaoEstoque() {//teste se a quantidade atual em estoque é menor que o estoque mínimo
+		if (this.quantidadeAtual < this.estoqueMinimo) {
+			this.quantidadeAtual = this.estoqueMaximo;
+		}
+	}
+
+	public abstract void venda();
+
+	public void setQuantidadeAtual(int itensVendidos) {// Atualiza a quantidade de itens em estoque atual.
+
+		this.quantidadeAtual -= itensVendidos;
+	}
+
+	public int getEstoqueMinimo() {
+		return estoqueMinimo;
+	}
+
+	public int getEstoqueMaximo() {
+		return estoqueMaximo;
+	}
+
+	public int getQuantidadeAtual() {
+		return quantidadeAtual;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+}
